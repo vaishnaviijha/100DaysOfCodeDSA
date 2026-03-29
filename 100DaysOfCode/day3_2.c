8bd8cee07f7ec8f5d4fd212e2db6567cecfd8dd3
vaishnaviijha <vaishnavi.29048@stu.upes.ac.in>
2026-03-29 13:20:55 +0530

    Updated day3_2.c solution

--- 100DaysOfCode/day3_2.c
+++ 100DaysOfCode/day3_2.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 
+int findMissing(int arr[], int size) {
+    int n = size + 1;
+    
+    int expected_sum = n * (n + 1) / 2;
+    int actual_sum = 0;
+
+    for(int i = 0; i < size; i++) {
+        actual_sum += arr[i];
+    }
+
+    return expected_sum - actual_sum;
+}
+
 int main() {
-    int n;
-    scanf("%d", &n);
+    int arr[] = {1, 2, 3, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    int prices[n];
-
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &prices[i]);
-    }
-
-    int min_price = prices[0];
-    int max_profit = 0;
-
-    for(int i = 1; i < n; i++) {
-        if(prices[i] < min_price) {
-            min_price = prices[i];
-        } else {
-            int profit = prices[i] - min_price;
-            if(profit > max_profit) {
-                max_profit = profit;
-            }
-        }
-    }
-
-    printf("%d", max_profit);
-
+    printf("Missing number: %d", findMissing(arr, size));
     return 0;
 }
